a8803c3e575be3dd4aedf135630890248f4858e2
bullbesh <babulehaha@gmail.com>
2023-05-30 00:33:31 +0300

    Add first case in main switch structure

--- coursework/main.cpp
+++ coursework/main.cpp
@@ -1,34 +1,56 @@
 #include "functions.h"
 
 
-int userChoice;
-void generalMenu() {
-    cout << "Загружена база данных по умолчанию\n\n";
+int _stateMenu;
+void Menu() {
     cout << "Выберите действие:\n";
     cout << "1 - Открыть файл\n";
     cout << "2 - Просмотр данных\n";
     cout << "3 - Записать файл\n";
     cout << "4 - Записать в новый файл\n";
     cout << "5 - Выход из программы\n\n> ";
-    cin >> userChoice;
+    cin >> _stateMenu;
 };
 
 int main() {
-    // SetConsoleCP(1251);
-    // SetConsoleOutputCP(1251);
-    // system ("chcp 1251");
+    system("chcp 65001");
+    Menu();
 
-    generalMenu();
+    int _actions, amountOfData = 0;
+    string fileName;
 
-    int action, dataAmount = 0;
-    char filename;
+    Data* d = new Data[amountOfData];
 
-    Table* Data = new Table[dataAmount];
-
-    while (userChoice != 0) {
-        switch (userChoice) {
+    while (_stateMenu != 5) {
+        switch (_stateMenu) {
             case 1:
-                
+                system("cls");
+                cout << "Выберите, как записывать данные? (1 - вручную, 0 - из файла)\n\n> ";
+                cin >> _actions;
+                system("cls");
+
+                if (_actions == 1) {
+                    DataEntry(d, amountOfData);
+                }
+
+                else {
+                    cout << "Введите название файла:\n\n>";
+                    cin >> fileName;
+                    ReadingData(d, amountOfData, fileName);
+                }
+
+                system("pause");
+                system("cls");
+                Menu();
+                break;
+
+            default:
+                cout << "Введены некорректные данные\n";
         }
+
+            system("pause");
+            system("cls");
+            Menu();
+            break;
     }
 }
