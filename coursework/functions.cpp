bd102e71d46feec919f75aeabce413091a8e0725
bullbesh <babulehaha@gmail.com>
2023-05-30 13:19:37 +0300

    Remove comments with old theme

--- coursework/functions.cpp
+++ coursework/functions.cpp
@@ -1,17 +1,6 @@
 #include "functions.h"
 
 
-// Table view:
-// Code | Name | Genre | Release | Rating
-
-// struct Product {
-//     {1, "What's Eating Gilbert Grape", "Drama", 1993, 8.4},
-//     {2, "Se7en", "Triller", 1995, 9.8},
-//     {3, "Fight Club", "Triller", 1999, 8.6},
-//     {4, "American Psycho", "Triller", 2000, 7.1},
-//     {5, "Catch Me If You Can", "Criminal", 2002, 8.9},
-// }
-
 void DataEntry(Data* (&d), int& n) {
     cout << "Введите количество данных на ввод:\n\n> ";
     cin >> n;
